a6cd75522e5510bbfd1e7e9b189a9246e520d538
oliver <oliver.ely14@gmail.com>
2022-04-01 16:01:09 +0100

    added write button

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import {Text, TouchableOpacity, View, StyleSheet, TextInput} from 'react-native';
 import NfcManager, {NfcTech, Ndef} from 'react-native-nfc-manager';
 
 NfcManager.start();
@@ -9,7 +9,6 @@ function App() {
     try {
       await NfcManager.requestTechnology(NfcTech.Ndef);
       const tag = await NfcManager.getTag();
-      console.log('big yikes');
       console.warn(
         'Tag found',
         Ndef.text.decodePayload(tag.ndefMessage[0].payload),
@@ -20,11 +19,42 @@ function App() {
       await NfcManager.cancelTechnologyRequest();
     }
   }
+
+  async function writeNdef({type, value}) { 
+    let result = false;
+
+    try {
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+
+      console.log(userInput);
+      const bytes = Ndef.encodeMessage([Ndef.textRecord('lol')]);
+
+      if (bytes) {
+        await NfcManager.ndefHandler.writeNdefMessage(bytes);
+        result = true;
+      }
+    } catch (ex) {
+      console.warn(ex);
+    } finally {
+      NfcManager.cancelTechnologyRequest();
+    }
+    return result;
+  }
+
   return (
     <View style={styles.wrapper}>
-      <TouchableOpacity style={styles.button} onPress={readNdef}>
-        <Text style={{color: 'white', fontSize: 15}}>READ</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity style={styles.button} onPress={readNdef}>
+          <Text style={{color: 'white', fontSize: 15}}>READ</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.buttonWrapper}>
+        <TouchableOpacity style={styles.button} onPress={writeNdef}>
+          <Text style={{color: 'white', fontSize: 15}}>WRITE</Text>
+        </TouchableOpacity>
+      </View>
+      <TextInput style={styles.input} placeholder="Input text"/>
+      <Text>{text}</Text>
     </View>
   );
 }
@@ -42,6 +72,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#2196f3',
     borderRadius: 3,
   },
+  buttonWrapper: {
+    margin: 15,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ffffff',
+    width: 200,
+    padding: 10,
+  }
 });
 
 export default App;
